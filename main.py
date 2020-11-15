e76e6445dea7edaaf2d12d581979ab8a4670ed04
marcoistasy <marco.istasy@mail.utoronto.ca>
2020-11-14 21:28:28 -0500

    updated script to flatten json

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#%%
+# %%
+import collections
 import requests
 import time
 import json
+
 
 class OXFORD_API():
 
@@ -14,8 +16,10 @@ class OXFORD_API():
         self.fields = fields
         self.strict = strict
 
-        self.entries_root = 'https://od-api.oxforddictionaries.com/api/v2/entries/{}/REPLACE_TEXT?fields={}&strictMatch'.format(self.language, self.fields, self.strict)
-        self.lemmas_root = 'https://od-api.oxforddictionaries.com/api/v2/lemmas/{}/REPLACE_TEXT'.format(self.language)
+        self.entries_root = 'https://od-api.oxforddictionaries.com/api/v2/entries/{}/REPLACE_TEXT?fields={}&strictMatch={}'.format(
+            self.language, self.fields, self.strict)
+        self.lemmas_root = 'https://od-api.oxforddictionaries.com/api/v2/lemmas/{}/REPLACE_TEXT'.format(
+            self.language)
 
     def get(self, word):
         # send request to api for word
@@ -25,37 +29,52 @@ class OXFORD_API():
 
         try:
             # request the OXFORD API for the definition
-            response = requests.get(entry_url, headers={"app_id": self.app_id, "app_key": self.app_key})
+            response = requests.get(
+                entry_url, headers={"app_id": self.app_id, "app_key": self.app_key})
             response.raise_for_status()
         except requests.exceptions.HTTPError:
             # if request failed, rerun with lemma
-            response = requests.get(lemma_url, headers={"app_id": self.app_id, "app_key": self.app_key})
+            response = requests.get(
+                lemma_url, headers={"app_id": self.app_id, "app_key": self.app_key})
 
         return Word(response.json())
 
 
 class Word():
-    
+
     def __init__(self, raw_text):
         self.raw_text = raw_text
 
     @property
-    def text(self):
-        return self.raw_text['results']
+    def entries(self):
+        return self.raw_text['results'][0]['lexicalEntries']
+
+    @property
+    def flattened_entries(self):
+        return [flatten(entry) for entry in self.entries]
+
+
+def flatten(data):
+
+    obj = collections.OrderedDict()
+
+    def recurse(data, key=""):
+        if isinstance(data, list):
+            for i in range(len(data)):
+                recurse(data[i], '{}.{}'.format(key, i) if key else str(i))
+        elif isinstance(data, dict):
+            for k, v in data.items():
+                recurse(v, '{}.{}'.format(key, k) if key else k)
+        else:
+            obj[key] = data
+
+    recurse(data)
+
+    return obj
 
 
 app_id = "73629f11"
 app_key = "e3713b7c303a270fb1827a72a4c233b3"
 dictionary = OXFORD_API(app_id, app_key)
 
-word = dictionary.get('hierophant')
-
-#%%
-data=word.text
-print(data)
-
-# %%
-print(sasdfadf)
-# print(json.dumps(x, indent=4))
-
-# %%
+word = dictionary.get('obverse')
