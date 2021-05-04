599837c995cee471505d0ddd2b700bf7a6e2f281
marco istasy <istasym@gmail.com>
2021-05-04 14:48:19 -0400

    refactoring

--- word.py
+++ word.py
@@ -44,24 +44,30 @@ class Entry:
 class Sense:
     # class to hold a given sense in an entry
 
-    def __init__(self, definition, domain=None, subsense=None, example=None):
+    def __init__(self, definition, domains=None, subsense=None, example=None, registers=None):
         self.definition = definition
-        self.domain = domain
+        self.domains = domains
         self.subsense = subsense
         self.example = example
+        self.registers = registers
 
     def add_supplementary_information(self, supplementary_information):
 
         # domain
         if 'domains' in supplementary_information:
-            self.domain = str(supplementary_information['domains'][0]['text'])
+            self.domains = [i['text'] for i in supplementary_information['domains']]
 
         # example
         if 'examples' in supplementary_information:
             self.example = str(
                 supplementary_information['examples'][0]['text'])
 
+        # registers
+        if 'registers' in supplementary_information:
+            self.registers = [i['text'] for i in supplementary_information['registers']]
+
         # subsense
         if 'subsenses' in supplementary_information:
             self.subsense = str(
                 supplementary_information['subsenses'][0]['definitions'][0])
+
