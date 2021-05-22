7b9a6577c880f053ea63be7e6d2dece7ebe569dc
marco istasy <istasym@gmail.com>
2021-05-22 00:09:00 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 # %%
 
+import requests
 from oxford_api import OxfordAPI
 
-id = "73629f11"
-key = "e3713b7c303a270fb1827a72a4c233b3"
-dictionary = OxfordAPI(id, key)
+# oxford_id = "73629f11"
+# oxford_key = "e3713b7c303a270fb1827a72a4c233b3"
+# dictionary = OxfordAPI(oxford_id, oxford_key)
 
-word = dictionary.get('idiopathic')
+# word = dictionary.get('translations')
 
-for i in word.entries:
-    print(i.definition, i.domain, i.category, i.example, i.subsense)
+#%%
+# print(word.entries[0].senses[0].definition)
+
+remnote_key = '59317628adc125d07aed2e82271ee400'
+remnote_id = 'bmMEYKHZ3rMs3fH34'
+page_id = 'nbMLTanczSK6zx6iv'
+remnote_create = 'https://api.remnote.io/api/v0/create'
+
+
+
+#%%
+
+input = {'apiKey': remnote_key, 'userId': remnote_id, 'parentId': page_id,
+  'text': 'My new Rem #[[Extra Card Detail]]',
+  'positionAmongstSiblings': 0,
+}
+x = requests.post(remnote_create, input)
+print(x.text)
+
+
 
 # %%
